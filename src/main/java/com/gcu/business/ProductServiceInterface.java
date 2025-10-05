6ddfe7c362681f6b5428a953aec73a4069d28a5c
hgbryant <huntergb1@icloud.com>
2025-10-04 21:10:35 -0500

    Updated code for Milestone3
    Updated ProductService.java
    Updated ProductServiceInterface.java
    Updated ProductModel.java

--- src/main/java/com/gcu/business/ProductServiceInterface.java
+++ src/main/java/com/gcu/business/ProductServiceInterface.java
@@ -1,10 +1,16 @@
 package com.gcu.business;
 
 import java.util.List;
+
 import com.gcu.model.ProductModel;
 
 public interface ProductServiceInterface {
-    // TODO: Define createProduct(ProductModel product)
 
-    // TODO: Define getAllProducts() returning List<ProductModel>
+    //Defines createProduct(ProductModel product)
+
+        void createProduct(ProductModel product);
+
+        List<ProductModel> getAllProducts();
+    
+    // Defines getAllProducts() returning List<ProductModel>
 }
--- src/main/java/com/gcu/model/ProductModel.java
+++ src/main/java/com/gcu/model/ProductModel.java
@@ -1,10 +1,54 @@
 package com.gcu.model;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class ProductModel {
 
-    // TODO: Add fields: id (Long), name (String), description (String), price (double)
+    // Adds fields: id (Long), name (String), description (String), price (double)
+    // Adds validation annotations (@NotBlank, @Size, @Min)
+    
+    private Long id;
 
-    // TODO: Add validation annotations (@NotBlank, @Size, @Min)
+    @NotBlank(message = "Name is required")
+    @Size(min = 2, max = 50, message = "Name must be between 2 and 50 characters")
+    private String name; 
 
-    // TODO: Add getters and setters
+    @NotBlank(message = "Description is required")
+    @Size(min = 5, max = 200, message = "Description must be between 5 and 200 characters")
+    private String description;
+
+    @Min(value = 0, message = "Price must be positive")
+    private double price;
+    
+    // Adds getters and setters
+    
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }

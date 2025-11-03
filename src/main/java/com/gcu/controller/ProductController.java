bcbeb9fa5673a26874313a8681443d3cef777d6a
Daniel Louis <DLouis4@my.gcu.edu>
2025-11-02 21:08:20 -0500

    complete from Milestone 7

--- src/main/java/com/gcu/controller/ProductController.java
+++ src/main/java/com/gcu/controller/ProductController.java
@@ -5,12 +5,9 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ModelAttribute;
